ac25848f1df16b9303ad384e4081a4de82eb32e5
Quang Thành <thanh.dangquang28@gmail.com>
2024-07-25 17:49:44 +0700

    feat: add mint nft and update ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,37 +5,26 @@ import { Account, PublicKey, Aptos, AptosConfig, Network } from "@aptos-labs/ts-
 import { InputTransactionData } from "@aptos-labs/wallet-adapter-core";
 import { useWallet } from "@aptos-labs/wallet-adapter-react";
 import "@aptos-labs/wallet-adapter-ant-design/dist/index.css";
+import { mint_nft } from "@/constant/constant";
 
 const aptosConfig = new AptosConfig({ network: Network.DEVNET });
 const aptos = new Aptos(aptosConfig);
-const APTOS_COIN = "0x1::aptos_coin::AptosCoin";
+
+type Tnft = {
+  current_token_data: {
+    current_collection: {
+      collection_name: string,
+      description: string,
+      uri: string
+    }
+  }
+}[]
 
 export default function Home() {
 
   const { account, network, signAndSubmitTransaction } = useWallet();
   const [balance, setBalance] = useState<number>(0)
   const [loading, setLoading] = useState(true)
-
-  // useEffect(() => {
-  //   const fetchList = async () => {
-  //     if (!account) return [];
-  //     // change this to be your module account address
-  //     const moduleAddress = "0xcbddf398841353776903dbab2fdaefc54f181d07e114ae818b1a67af28d1b018";
-  //     try {
-  //       const todoListResource = await aptos.getAccountResource(
-  //         {
-  //           accountAddress: account?.address,
-  //           resourceType: `${moduleAddress}::todolist::TodoList`
-  //         }
-  //       );
-  //       setAccountHasList(true);
-  //     } catch (e: any) {
-  //       setAccountHasList(false);
-  //     }
-  //   };
-
-  //   fetchList();
-  // }, [account?.address]);
 
   const [accountHasList, setAccountHasList] = useState<boolean>(false);
 
@@ -69,58 +58,14 @@ export default function Home() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [balance, account])
 
-  const sendTransaction = async () => {
-    if (account) {
-      const transaction = await aptos.transaction.build.simple({
-        sender: account.address,
-        data: {
-          // All transactions on Aptos are implemented via smart contracts.
-          function: "0x1::aptos_account::transfer",
-          functionArguments: ['0x115c1e9c37f777226ec3230b78473ea401df6f2ea6f7229e0c1c71938e61f30d', 100],
-        },
-      });
-
-      console.log("Built the transaction!")
-
-      console.log("\n === 2. Simulating Response (Optional) === \n")
-      const [userTransactionResponse] = await aptos.transaction.simulate.simple({
-        signerPublicKey: account.publicKey as unknown as PublicKey,
-        transaction,
-      });
-      console.log(userTransactionResponse)
-
-      // 3. Sign
-      console.log("\n=== 3. Signing transaction ===\n");
-      const senderAuthenticator = aptos.transaction.sign({
-        signer: account as unknown as Account,
-        transaction,
-      });
-      console.log("Signed the transaction!")
-
-      // 4. Submit
-      console.log("\n=== 4. Submitting transaction ===\n");
-      const submittedTransaction = await aptos.transaction.submit.simple({
-        transaction,
-        senderAuthenticator,
-      });
-
-      console.log(`Submitted transaction hash: ${submittedTransaction.hash}`);
-
-      // 5. Wait for results
-      console.log("\n=== 5. Waiting for result of transaction ===\n");
-      const executedTransaction = await aptos.waitForTransaction({ transactionHash: submittedTransaction.hash });
-      console.log(executedTransaction)
-    }
-  }
-
   const onSignAndSubmitTransaction = async () => {
     if (!account) return;
     const transaction: InputTransactionData = {
       sender: account.address,
       data: {
-        function: "0x1::aptos_account::transfer_coins",
-        typeArguments: [APTOS_COIN],
-        functionArguments: ['', 1000], // 1 is in Octas
+        function: `${mint_nft.publisher}::${mint_nft.module_name}::${mint_nft.fun}`,
+        typeArguments: [],
+        functionArguments: ['hihi', 'hihu', 'hoho'], // 1 is in Octas
       },
     };
     try {
@@ -128,34 +73,65 @@ export default function Home() {
       await aptos.waitForTransaction({
         transactionHash: response.hash,
       });
-      // toast({
-      //   title: "Success",
-      //   description: <TransactionHash hash={response.hash} network={network} />,
-      // });
+
+      getNft()
     } catch (error) {
       console.error(error);
     }
   };
 
+  const [nfts, setListNfts] = useState<Tnft>([])
+
+  const getNft = async () => {
+    if (account) {
+      const tokens = await aptos.getAccountOwnedTokens({ accountAddress: account.address });
+
+      setListNfts(
+        tokens as any
+      )
+
+    }
+  }
+
+  useEffect(() => {
+    if (account) getNft()
+  }, [account])
+
   return (
-    <>
-      <div className="h-[1000px] bg-white w-dvh flex flex-col items-center gap-5 m-5">
+    <div className="min-h-dvh bg-black w-dvh flex flex-col items-center gap-5 m-auto">
+      <div className="flex justify-end p-4 bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%0 w-full">
         <WalletSelector />
-        {account && <div className="flex flex-col gap-5">
-          <div id="action_button" className="flex items-center justify-center gap-1">
-            <button onClick={() => getFaucet()} className="rounded-xl border bg-orange-300 p-3 hover:opacity-70 text-green-800">Faucet</button>
-            <button onClick={() => onSignAndSubmitTransaction()} className="rounded-xl border bg-orange-300 p-3 hover:opacity-70 text-green-800">Send transaction</button>
-          </div>
-          <div id="accoutn-info">
-            <p><b>Address:</b> {account.address}</p>
-            <p><b>Balance:</b> {loading ? `loading...` : balance} APT</p>
-            <p><b>PublicKey:</b> {account.publicKey}</p>
-            <p><b>minKeysRequired:</b> {account.minKeysRequired}</p>
-            <p><b>ansName:</b> {account.ansName}</p>
-          </div>
-        </div>
-        }
       </div>
-    </>
+      {account && <div className="flex flex-col pt-4 gap-10">
+        <div id="action_button" className="flex items-center font-medium text-white justify-center gap-4">
+          <button onClick={() => getFaucet()} className="rounded-xl font-medium text-white border bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 px-6 py-3 hover:opacity-70">Faucet</button>
+          <button onClick={() => onSignAndSubmitTransaction()} className="rounded-xl font-medium text-white border bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 px-6 py-3 hover:opacity-70">Min NFT</button>
+        </div>
+        <div id="accoutn-info" className="text-white text-xl">
+          <p><b>Address:</b> {account.address}</p>
+          <p><b>Balance:</b> {loading ? `loading...` : balance} APT</p>
+          <p><b>PublicKey:</b> {account.publicKey}</p>
+        </div>
+      </div>
+      }
+      <div className="grid grid-cols-2 gap-5 h-full">
+        {
+          nfts.length > 0 ? nfts.map((nft, index) => (<div className="flex justify-between flex-col w-[200px] h-[200px] p-5 text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:scale-110 duration-200" key={index}>
+            <p className="w-full truncate">
+              {nft.current_token_data.current_collection.collection_name}
+            </p>
+            <p className="w-full truncate">
+              {nft.current_token_data.current_collection.description}
+            </p>
+            <p className="w-full truncate">
+              {nft.current_token_data.current_collection.uri}
+            </p>
+          </div>)
+          ) :
+            <div>Empty</div>
+        }
+
+      </div>
+    </div>
   );
 }
---
+++ src/constant/constant.tsx
@@ -0,0 +1,5 @@
+export const mint_nft = {
+    publisher: '0xed858c08e6123ad93bea04a51fe4bc0f194774a65551a51fc83ce0c3f7001e51',
+    module_name: 'my_demo',
+    fun: 'mint_card_nft'
+}
